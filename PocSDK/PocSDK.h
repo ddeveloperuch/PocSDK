07cc5cadca4baed41d6d4fd6039f136cc9299ea7
Dmitry Grigansky <dgrigansky@malwarebytes.com>
2017-08-23 13:16:54 -0400

    Add new import statement.

--- PocSDK/PocSDK.h
+++ PocSDK/PocSDK.h
@@ -17,3 +17,5 @@ FOUNDATION_EXPORT const unsigned char PocSDKVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <PocSDK/PublicHeader.h>
 
 #import <PocSDK/Protection.h>
+
+#import <PocSDK/ScanManager.h>
